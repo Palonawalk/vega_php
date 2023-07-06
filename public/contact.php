b7c77ede0775833dbcdb6d80400ff74e15810f3d
Palona <lainedaridan.as@gmail.com>
2023-07-06 12:32:43 +0200

    Mise en place du contrôleur frontal

---
+++ public/contact.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <nav>
+            <a href="index.php">Accueil</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    <h1>Contact</h1>
+</body>
+</html>
